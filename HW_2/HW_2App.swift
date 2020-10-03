64a2c1c3b1fd6324b4996ff72f6a3ba8e2fd65df
xxxPatty <L224749391@gmail.com>
2020-10-03 12:17:16 +0800

    final2

---
+++ HW_2/HW_2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2App.swift
+//  HW_2
+//
+//  Created by 林湘羚 on 2020/10/1.
+//
+
+import SwiftUI
+
+@main
+struct HW_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
